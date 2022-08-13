ba69906a351c149a6c5921a5f0b3f3e2acc77045
Dor Lugasi <dorlugasigal@gmail.com>
2022-08-13 12:26:43 +0300

    remove dist

--- dist/background.js
+++
@@ -1,9 +0,0 @@
-var store = {};
-
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.type === 'from_content_script') {
-    store = message.content;
-  } else if (message.type === 'from_popup') {
-    sendResponse(store);
-  }
-});
